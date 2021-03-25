4952cf204d84173c055954fc86f7e886b23efa38
Ajay Mishra <Ajaym6405@gmail.com>
2021-03-25 19:12:20 +0530

    Update snake_game1.js

--- snake_game1.js
+++ snake_game1.js
@@ -8,37 +8,74 @@ function setup() {
 let stepSize = 20;
 let food = [100, 100];
 var snake = [[0, 0], [0, stepSize], [0, stepSize * 2]];
+let direction = 'U';
+let number = 0;
+let inc = 1;
+function oppositeTo(dir){
+  switch(dir){
+    case 'U' : return 'D';
+    case 'D' : return 'U';
+    case 'R' : return 'L';
+    case 'L' : return 'R';
+  }
+}
 function draw() {
   translate(300, 300);
-  background(121, 43, 39);
+  background(121, 43, 39)
+  fill(56, 60, 34);
+  rect(0, 0, 580, 580); 
   fill(100, 50, 150);
-	noStroke();
-  ellipse(food[0], food[1], 20, 20);
+  noStroke();
+  number += inc;
   
-  
+  ellipse(food[0], food[1], 0.75*(number % 20) + 20, 0.75*(number % 20) + 20);
+  if(number > 19){
+    inc = -1;
+  }
+  else if(number < 1){
+    inc = 1;
+  }
   let [x, y] = snake[0];
+  if(value == oppositeTo(direction)){
+    value = direction;
+  }
   if(value != undefined){
     x += value == "L" ? -stepSize : value == "R" ? stepSize : 0;
     y += value == "U" ? -stepSize : value == "D" ? stepSize : 0;
-    if(x < -300)        x = 300;
-    if(x > 300)         x = -300;
-    if(y < -300)       y = 300;
-    if(y > 300)         y = -300;
+    direction = value;
+    if(x<-300||x>300|y<-300||y>300||snake.slice(1).some(([a, b]) => a == x && b == y)){
+      background(255);
+      textSize(50);
+      text("GAME OVER", -140, 0);
+      noLoop();
+    }
+
     snake.unshift([x, y]);
     if(x == food[0] && y == food[1]){
-      food = [-300 + Math.floor(300 * Math.random()), -300 + Math.floor(300 * Math.random())];
+      food = [-280 + Math.floor(280 * Math.random()), -280 + Math.floor(280 * Math.random())];
       food[0] = Math.floor(food[0] / stepSize) * stepSize;
       food[1] = Math.floor(food[1] / stepSize) * stepSize;
     }
     else{
       snake.pop();
     }
+    
   }
+  
+  fill(255, 0, 0);
+  let [hx, hy] = snake[0];
+  ellipse(hx, hy, 20, 20);
   fill(0);
-  for(let [i, j] of snake){
+  for(let [i, j] of snake.slice(1)){
     rect(i, j, 20, 20);
   }
-    
+  
+  textSize(10);
+  fill(0, 0, 255);
+  noFill();
+  stroke(0);
+  rect(-220, -250, 100, 40);
+  text("Score : " + snake.length * 5, -250, -250);
 }
 
 function keyPressed() {
